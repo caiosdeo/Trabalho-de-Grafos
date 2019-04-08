8d975ff962fef401b32f36028b193dfe60a59063
caiosdeo <caiosdeo@hotmail.com>
2019-04-08 11:13:40 -0300

    resolved problems when compiling

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[]) {
     //Verificação se todos os parâmetros do programa foram entrados
     if (argc != 6) {
 
-        cout << "ERROR: Expecting: ./<program_name> <input_file> <output_file> <directed> <weighted_edge> <weighted_node>"
+        cout << "ERROR: Expecting: ./<program_name> <input_file> <output_file> <directed> <weighted_edge> <weighted_node>";
         return -1;
 
     }
@@ -24,43 +24,47 @@ int main(int argc, char const *argv[]) {
     //Criação do arquivo de saída
     ofstream output_file(argv[2]);
 
+    bool directed;
+    bool weighted_edge;
+    bool weighted_node;
+    //Variável para pegar número de vértices
+    int order;
+
     //Verificando se o grafo é direcionado
     if (argv[3] == 0) {
 
-        bool directed = false;
+        directed = false;
 
     }else {
 
-        bool directed = true;
+        directed = true;
 
     }
 
     //Verificando se as arestas são ponderadas
     if (argv[4] == 0) {
 
-        bool weighted_edge = false;
+        weighted_edge = false;
 
     }else {
 
-        bool weighted_edge = true;
+        weighted_edge = true;
     }
 
     //Verificando se os nós são ponderados
     if (argv[5] == 0) {
 
-        bool weighted_node = false;
+        weighted_node = false;
 
     }else {
 
-        bool weighted_node = true;
+        weighted_node = true;
     }
 
     //Leitura de arquivo:
     if(input_file.is_open()){
 
-        //Variável para pegar número de vértices
-        int order;
-        getline(input_file, order);
+        input_file >> order;
 
     }else {
 
