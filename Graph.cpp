c3d60f6a4c4e40cc4deb3bb7901687b7716c2181
caiosdeo <caiosdeo@hotmail.com>
2019-05-14 22:33:36 -0300

    moved specific methods

--- Graph.cpp
+++ Graph.cpp
@@ -358,71 +358,6 @@ bool Graph::depthFirstSearch(int initialId, int targetId)
     return auxDepthFirstSearch(initialId, targetId, visited);
 }
 
-//Used Kosaraju Algorithm
-int* Graph::stronglyConnectedComponents(){
-
-    if(!this->connectedGraph()){
-        return nullptr;
-
-    }else{
-
-        //Stack for visited nodes
-        stack<int> explored;
-        bool *visited = new bool[order];
-        //Vector with the stronglyConnectedComponents
-        int *sc = new int[this->order];
-        //Int to label the SCCs
-        int label = 0;
-
-        if (this->first_node == nullptr || this->last_node == nullptr){
-            return 0;
-        }
-
-        //Begin the DFS with every node as unvisited
-        for (int i = 0; i < order; i++){
-            visited[i] = false;
-        }
-
-        //Function to fill the stack with DFS visited nodes in post order
-        exploreOrder(this->first_node->getId(), this->first_node->getId(), visited, &explored);
-
-        //Confirms that all the nodes were included in the stack
-        for (Node *n = this->first_node; n != nullptr; n = n->getNextNode())
-            if (visited[indexForNodes(n->getId())] == false)
-                exploreOrder(n->getId(), n->getId(), visited, &explored);
-
-        //Create a reverse graph
-        Graph *gT = this->getTranspose();
-
-        //Define every node as unvisited for the second DFS
-        for (int i = 0; i < order; i++)
-        {
-            visited[i] = false;
-        }
-
-        //DFS on the reverse graph by the stack order as its is running we find the SCCs of the graph
-        while (explored.empty() == false)
-        {
-
-            int v = explored.top();
-            explored.pop();
-
-            //DFS on the reverse graph that will find the SCCs
-            if (visited[gT->indexForNodes(v)] == false){
-
-                gT->auxComponents(v, v, visited, sc, label);
-                label++;
-
-            }
-
-        }
-
-        return sc;
-
-    }
-
-}
-
 void Graph::breadthFirstSearch(ofstream &output_file)
 {
     int i = 0;
@@ -474,6 +409,39 @@ Graph *Graph::getComplement()
     }
 
     return complement;
+}
+
+//A function that returns a reverse graph, which is a graph which the arcs have opposite directions to the original graph
+Graph* Graph::getTranspose(){
+
+    //Create the reverse as the same order
+    Graph *gT = new Graph(this->order, this->directed, this->weighted_edge, this->weighted_node);
+
+    for (Node *n = this->first_node; n != nullptr; n = n->getNextNode())
+    {
+        for (Edge *e = n->getFirstEdge(); e != nullptr; e = e->getNextEdge())
+        {
+            gT->makeGraph(e->getTargetId(), n->getId(), e->getWeight());
+        }
+    }
+    return gT;
+}
+
+//A function that returns a subjacent of a directed graph, which is a graph which the arcs have opposite directions to the original graph
+Graph* Graph::getSubjacent()
+{
+
+    //Create the subjacent with the same order
+    Graph *gS = new Graph(this->order, 0, this->weighted_edge, this->weighted_node);
+
+    for (Node *n = this->first_node; n != nullptr; n = n->getNextNode())
+    {
+        for (Edge *e = n->getFirstEdge(); e != nullptr; e = e->getNextEdge())
+        {
+            gS->makeGraph(n->getId(), e->getTargetId(), e->getWeight());
+        }
+    }
+    return gS;
 }
 
 int* Graph::degreeSequence(){
@@ -539,34 +507,6 @@ bool Graph::connectedGraph()
     }
 }
 
-//function that finds the connected component of a node
-int* Graph::connectedComponents(){
-
-    bool *visited = new bool[this->order];
-    //Vector with the connectedComponents
-    int *cc = new int[this->order];
-    //Int to label the CCs
-    int label = 0;
-
-    //Begin the DFS with every node as unvisited
-    for (int i = 0; i < order; i++){
-        visited[i] = false;
-    }
-
-    //Label all nodes from the beginning
-    for (Node* aux = this->getFirstNode(); aux != nullptr; aux = aux->getNextNode()){
-
-        if(visited[indexForNodes(aux->getId())] == false){
-
-            this->auxComponents(aux->getId(), this->getLastNode()->getId(), visited, cc, label);
-            label++;
-
-        }
-
-    }
-    return cc;
-}
-
 //Auxiliar methods
 bool Graph::hasCircuit()
 {
@@ -596,38 +536,6 @@ bool Graph::hasCircuit()
 
     //If all the labels are different among them, this graph does not have a circuit
     return false;
-}
-//A function that returns a reverse graph, which is a graph which the arcs have opposite directions to the original graph
-Graph* Graph::getTranspose(){
-
-    //Create the reverse as the same order
-    Graph *gT = new Graph(this->order, this->directed, this->weighted_edge, this->weighted_node);
-
-    for (Node *n = this->first_node; n != nullptr; n = n->getNextNode())
-    {
-        for (Edge *e = n->getFirstEdge(); e != nullptr; e = e->getNextEdge())
-        {
-            gT->makeGraph(e->getTargetId(), n->getId(), e->getWeight());
-        }
-    }
-    return gT;
-}
-
-//A function that returns a subjacent of a directed graph, which is a graph which the arcs have opposite directions to the original graph
-Graph* Graph::getSubjacent()
-{
-
-    //Create the subjacent with the same order
-    Graph *gS = new Graph(this->order, 0, this->weighted_edge, this->weighted_node);
-
-    for (Node *n = this->first_node; n != nullptr; n = n->getNextNode())
-    {
-        for (Edge *e = n->getFirstEdge(); e != nullptr; e = e->getNextEdge())
-        {
-            gS->makeGraph(n->getId(), e->getTargetId(), e->getWeight());
-        }
-    }
-    return gS;
 }
 
 //A function that fixate ordinal indexes for a node list
@@ -729,56 +637,6 @@ bool Graph ::auxBreadthFirstSearchVerify(int *verify, int size, int targetId)
     return verified; //Node not founded
 }
 
-// Kahn's algorithm adapted
-int *Graph::topologicalSort()
-{
-
-    // Verifies if the graph has a circuit or not
-    if (this->hasCircuit())
-        return nullptr;
-
-    else
-    {
-
-        int i = 0;
-        Edge *aux_edge;
-        Node *aux_node;
-        int *vec = new int(this->order); // Allocating the vector that will contains the topological sort
-        queue<Node *> topological_queue; // Declaring the auxiliar queue for the source nodes
-        // Searching for nodes with indegree equal to zero
-        for (aux_node = this->first_node; aux_node != nullptr; aux_node = aux_node->getNextNode())
-        {
-            // Verifies whether the indegree is equal to zero
-            if (aux_node->getInDegree() == 0)
-                topological_queue.push(aux_node); // Pushing the correct nodes in the queue
-        }
-        // Verifies if the queue is empty
-        while (!topological_queue.empty())
-        {
-
-            vec[i] = topological_queue.front()->getId();          // Pushing the id of the node to be popped of the queue
-            aux_edge = topological_queue.front()->getFirstEdge(); // Getting the first edge of the node to be popped
-            topological_queue.pop();                              // Popping the node
-            // Verifies whether an edge exists
-            while (aux_edge != nullptr)
-            {
-
-                aux_node = this->getNode(aux_edge->getTargetId()); // Picking up the neighboring node
-                aux_node->decrementInDegree();                     // Decrementing indegree
-                // Verifies whether the indegree is equal to zero
-                if (aux_node->getInDegree() == 0)
-                    topological_queue.push(aux_node);
-
-                aux_edge = aux_edge->getNextEdge();
-            }
-
-            i++;
-        }
-
-        return vec; // Returning the indexes correlated to the topological sort in a vector
-    }
-}
-
 // This function return the Node with the highest degree
 Node* Graph::getHighestDegreeNode() {
 
@@ -790,60 +648,4 @@ Node* Graph::getHighestDegreeNode() {
     }
 
     return highestDegreeNode;
-}
-
-
-// This function returns the Minimum Connected Dominant Set
-list<int> Graph::greedyMinimumConnectedDominantSet(){
-
-    /*
-    //Pointer with the node of the bigger degree
-    Node* sortedOutdegree = this->sortByOutDegree();
-    */
-
-    //List for the Minimum Connected Dominant Set
-    list<int> minimun_connected_dominant_set;
-    //Queue for visiting nodes
-    queue<Node*> toVisit;
-    //Vector to know if a node was visited and marking all as unvisited
-    bool *visited = new bool[this->order];
-    for(int i = 0; i < this->order; i++)
-        visited[i] = false;
-
-    Node* auxNode;
-
-    //The first node (change it to major outdegree, it is the first node at sortedOutdegree) is already visited
-    visited[this->indexForNodes(this->getFirstNode()->getId())] = true;
-    toVisit.push(this->getFirstNode());
-
-    while(!toVisit.empty()){
-
-        //Auxilar to the node in the queue front
-        auxNode = toVisit.front();
-        // Adding the node id in the solution list
-        minimun_connected_dominant_set.push_front(auxNode->getId());
-        //Then its is removed from the queue
-        toVisit.pop();
-
-        //Recur all of his adjacents
-        for(Edge* auxEdge = auxNode->getFirstEdge(); auxEdge != nullptr; auxEdge = auxEdge->getNextEdge()){
-
-            int targetId = auxEdge->getTargetId();
-
-            //If a adjacent was not visited it is marked as visited and added to the queue
-            if(!visited[indexForNodes(targetId)]){
-
-                visited[indexForNodes(targetId)] = true;
-                auxNode = this->getNode(targetId);
-                if(!auxNode->isLeaf(visited))
-                    toVisit.push(auxNode);
-
-            }
-
-        }
-
-    }
-
-    return minimun_connected_dominant_set;
-
 }
