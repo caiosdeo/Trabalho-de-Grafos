778e0b5e88b4e902444d0c16a306553ea448ee0f
eliascassis <eliascyrino@gmail.com>
2019-05-18 21:43:09 -0300

    first implementation of quickSort

--- Graph.cpp
+++ Graph.cpp
@@ -693,3 +693,30 @@ void swap(Node* x, Node* y){
     y = z;
 
 }
+
+int partition(Node** arr, int low, int high){ 
+    Node* pivot = arr[high]; 
+    int i = (low - 1);  
+  
+    for(int j = low; j <= high - 1; j++) { 
+
+        if(arr[j]->getOutDegree() <= pivot->getOutDegree()) { 
+            i++;    
+            swap(arr[i], arr[j]); 
+        } 
+    } 
+
+    swap(arr[i + 1], arr[high]); 
+    return (i + 1); 
+} 
+
+void quickSort(Node** arr, int low, int high) 
+{ 
+    if (low < high) {
+
+        int pi = partition(arr, low, high); 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+
+    } 
+} 
